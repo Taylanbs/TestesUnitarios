bef0680109d5ebd7beefce8f8ed5375e6b019980
Taylan Santos <taylanmf@gmail.com>
2024-01-10 18:17:45 -0300

    TestesUnitarios

---
+++ teste.cs
@@ -0,0 +1,73 @@
+// Projeto de Testes
+
+using System;
+using Xunit;
+
+namespace SeuNamespaceDeTestes
+{
+    public class ValidacoesListaTests
+    {
+        [Fact]
+        public void DeveRemoverNumerosNegativosDeUmaLista()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void DeveConterONumero9NaLista()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void NaoDeveConterONumero10NaLista()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void DeveMultiplicarOsElementosDaListaPor2()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void DeveRetornar9ComoMaiorNumeroDaLista()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void DeveRetornarOitoNegativoComoMenorNumeroDaLista()
+        {
+            // TODO: Implementar o teste
+        }
+    }
+
+    public class ValidacoesStringTests
+    {
+        [Fact]
+        public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void DeveContemAPalavraQualquerNoTexto()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void NaoDeveConterAPalavraTesteNoTexto()
+        {
+            // TODO: Implementar o teste
+        }
+
+        [Fact]
+        public void TextoDeveTerminarComAPalavraProcurado()
+        {
+            // TODO: Implementar o teste
+        }
+    }
+}
